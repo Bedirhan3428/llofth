2d1ba377deec32e10d5a20f3d85fdc46f9db1bc5
unknown <abimer2350@gmail.com>
2026-08-31 20:43:58 +0300

    fix: 100% stable startup via direct next bin execution

--- baslat.bat
+++ baslat.bat
@@ -1,37 +1,5 @@
 @echo off
 cd /d "%~dp0"
-title llofth • Yerel Medya Portalı
-cls
-
-:: Node.js kontrolu
-where node >nul 2>nul
-if %errorlevel% neq 0 (
-    echo [HATA] Node.js bilgisayarinizda bulunamadi!
-    echo Lutfen https://nodejs.org adresinden Node.js kurun.
-    echo.
-    pause
-    exit /b 1
-)
-
-:: Bagimlilik kontrolu
-if not exist "node_modules\" (
-    echo [*] Gerekli paketler yukleniyor (npm install)...
-    call npm install
-    echo.
-)
-
-:: Build kontrolu
-if not exist ".next\" (
-    echo [*] Ilk kurulum derlemesi yapiliyor (npm run build)...
-    call npm run build
-    echo.
-)
-
-:: Sunucuyu Baslat
+title llofth
 node lib\start.js
-
-if %errorlevel% neq 0 (
-    echo.
-    echo [Hata olustu veya sunucu kapandi]
-    pause
-)
+pause
